197279c8233c9015969145610986cdab06ba7a56
SawKyawtKayKhaing <maythaecho028@gmail.com>
2020-09-04 11:28:20 +0630

    hi

---
+++ app/Http/Controllers/DonarController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\donar;
+use Illuminate\Http\Request;
+
+class DonarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $donars=donar::all();
+        return view('donars.index',['donars'=>$donars]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('donar.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $donars=new donar();
+        $donars->name = $request->name;
+        $donars->gender=$request->gender;
+        $donars->address=$request->address;
+        $donars->phone=$request->phone;
+        $donars->age=$request->age;
+        $donars->date_of_birth=$request->dob;
+        $donars->member=$request->member;
+        $donars->save();
+        return redirect()->action('/donars');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
